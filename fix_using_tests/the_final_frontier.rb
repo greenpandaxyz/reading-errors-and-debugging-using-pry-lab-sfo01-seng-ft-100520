a70590adf8738c4f391b9210405bfde3740d2a36
Amanda D <amanda@depaula.xyz>
2020-09-13 23:53:45 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,17 @@
 require 'pry'
 
 def generate_star_date
+      binding.pry
   (rand(100000) + 400000) / 10.0
 end
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
+      binding.pry
 end
 
 def engage
   puts state_log(date)
+      binding.pry
   date = generate_star_date
-    binding.pry
 end
