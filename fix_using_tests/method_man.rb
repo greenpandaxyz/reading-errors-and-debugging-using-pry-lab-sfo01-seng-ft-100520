911555e4f181db1d202437d44c3d5b803dfed0b8
Amanda D <amanda@depaula.xyz>
2020-09-13 23:13:48 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,12 +1,9 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
-    binding.pry
-
+  start_game = "Hello #{player1} & #{playar2}"
 end
 
 def play_game(player2)
-  "#{player1} is better than #{player2}"
-    binding.pry
+  play_game = "#{player1} is better than #{player2}"
 end
