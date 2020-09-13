42c33e376d1126a44f890ecb78b152e6c96fc6b4
Amanda D <amanda@depaula.xyz>
2020-09-13 23:15:26 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,8 +2,10 @@
 
 def start_game(player1, player2)
   start_game = "Hello #{player1} & #{playar2}"
+  binding.pry
 end
 
 def play_game(player2)
   play_game = "#{player1} is better than #{player2}"
+  binding.pry
 end
